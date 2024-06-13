264248550d2ac5dcbbfa771a6db9d3547df6aa33
usamarazzaq0 <160162848+usamarazzaq0@users.noreply.github.com>
2024-06-13 22:57:58 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="user">
+    <sidebarVue/>
+    <div class="flex flex-col md:pl-64">
+      <TopHeader/>
+      <main>
+        <router-view/>
+      </main>
+    </di
+    TopHeader,
+},
+  data() {
+    return {};
+  },
+  methods:{
+   
+  },
+  computed: {
+    user() {
+      return this.$store.state.userData
+    }
+  }
+};
+</script>
+
+<style></style>
