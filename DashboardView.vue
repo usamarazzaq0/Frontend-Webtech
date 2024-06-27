827c6eb1d561578e3acd30168fcb90061d2ecbc4
usamarazzaq0 <160162848+usamarazzaq0@users.noreply.github.com>
2024-06-27 23:49:21 +0100

    Add files via upload

--- DashboardView.vue
+++ DashboardView.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="flex-1 py-6">
-    <div class="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
-      <h2
-        
 
-          });
-    },
   },
   created() {
     this.index();
